0f7972bbf68c5e9e84b4f01435f76bcff83ef510
vikas89tiwari67-lgtm <vikas89tiwari67@gmail.com>
2026-09-05 16:14:35 +0530

    Add files via upload
    array

---
+++ array.js
@@ -0,0 +1,39 @@
+// array
+let myArray = [1,2,3,4,5]
+// console.log(myArray);
+// console.log(myArray[0])
+// let myArray2 = new Array('a','b','c')
+// console.log(myArray2)
+// console.log(myArray2[0]);
+
+
+
+// array Method
+myArray.push(6)
+// myArray.pop(4)
+// myArray.unshift(0)
+
+
+// // myArray.shift() //it will remove first element 
+
+// console.log(myArray);
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(2));
+
+// let newarray = myArray.join()// joins array in string formate 
+
+// console.log(myArray);
+// console.log(newarray)
+// console.log(typeof newarray);
+
+// slice and splice
+console.log('a', myArray);
+
+let mya1 = myArray.slice(0,3)
+console.log('B', mya1)
+console.log('c', myArray);
+
+let mya2 = myArray.splice(0,3)
+console.log('d', mya2)
+console.log('e', myArray);
